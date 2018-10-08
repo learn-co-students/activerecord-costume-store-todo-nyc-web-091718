5c5a8d9bd40eb559901845e319401b3c561ece44
github username <github email address>
2018-10-08 00:48:40 -0400

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,3 +1,15 @@
+class CreateCostumes < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :image_url
+      t.integer :size
+      t.timestamps
+    end
+  end
+end
+
 # Create a class and inherit from ActiveRecord::Migration
 
 # By convention, the class name should match the part of the
@@ -5,5 +17,26 @@
 # 002_create_costumes.rb becomes class CreateCostumes
 
 # Define a change method in which to do the migration
-# In this change method, create columns with the correct names and 
+# In this change method, create columns with the correct names and
 # value types according to the spec
+
+
+# Costume
+#   has a name (FAILED - 1)
+#   has a price (FAILED - 2)
+#   has an image url (FAILED - 3)
+#   has a size (FAILED - 4)
+#   knows when it was created (FAILED - 5)
+#   knows when it was last updated (FAILED - 6)
+
+
+
+# class CreateCandies < ActiveRecord::Migration[4.2]
+#   def change
+#     create_table :candies do |t|
+#       t.string :name
+#       t.integer :calories
+#       t.timestamps
+#     end
+#   end
+# end
--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,24 @@
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
+      #t.timestamps
+    end
+  end
+end
 # Create your costume_stores migration here
+
+# CostumeStore
+#   has a name (FAILED - 7)
+#   has a location (FAILED - 8)
+#   has a costume inventory (FAILED - 9)
+#   has an employees count (FAILED - 10)
+#   knows if it's still in business or permanently closed (FAILED - 11)
+#   has an opening time (FAILED - 12)
+#   has a closing time (FAILED - 13)
